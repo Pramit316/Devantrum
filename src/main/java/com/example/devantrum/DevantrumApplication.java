833b09a2cf9c4174675bb52234d5d22764320963
pramit <bhattaraipramit07@gmail.com>
2026-05-25 20:22:58 +0545

    Initial commit

---
+++ src/main/java/com/example/devantrum/DevantrumApplication.java
@@ -0,0 +1,13 @@
+package com.example.devantrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevantrumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevantrumApplication.class, args);
+    }
+
+}
