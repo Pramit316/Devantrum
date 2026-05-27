b79d957c541d46731f094ffd6e557a995f6a1259
pramit <primeshipbas@gmail.com>
2026-05-27 21:21:38 +0545

    user controller and jpa connectivity

--- src/main/java/com/example/devantrum/user/controller/UserController.java
+++ src/main/java/com/example/devantrum/user/controller/UserController.java
@@ -1,4 +1,39 @@
 package com.example.devantrum.user.controller;
 
+import com.example.devantrum.user.entity.User;
+import com.example.devantrum.user.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/all")
+    public List<User> getAllUser(){
+        return userService.getAllUser();
+    }
+
+    @PostMapping("/")
+    public User  addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @GetMapping("get/{id}")
+    public Optional<User> getUserById(@PathVariable Long id){
+        return  userService.getUserById(id);
+    }
+
+    @PutMapping("update/{id}")
+    public User updateUser(@RequestBody User user){
+        return userService.updateUser(user);
+    }
 }
--- src/main/java/com/example/devantrum/user/service/UserService.java
+++ src/main/java/com/example/devantrum/user/service/UserService.java
@@ -1,4 +1,36 @@
 package com.example.devantrum.user.service;
 
+import com.example.devantrum.user.controller.UserController;
+import com.example.devantrum.user.entity.User;
+import com.example.devantrum.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User updateUser(User newUser) {
+        return userRepository.save(newUser);
+    }
 }
