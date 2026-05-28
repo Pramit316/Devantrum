ebbadd08b7cabacb5bf7437a9f7c25e21c937eac
pramit <primeshipbas@gmail.com>
2026-05-28 21:26:22 +0545

    added update post feature

--- src/main/java/com/example/devantrum/post/entity/Post.java
+++ src/main/java/com/example/devantrum/post/entity/Post.java
@@ -1,17 +1,24 @@
 package com.example.devantrum.post.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "post")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String shipped; // what I built today
